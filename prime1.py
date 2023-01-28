1d7766c0550632e5ea07429b2f2d622375306cb8
Pravin128 <121967042+Pravin128@users.noreply.github.com>
2023-01-28 11:02:42 +0530

    Create prime1.py

---
+++ prime1.py
@@ -0,0 +1,15 @@
+#prime numbers between two number taken from user
+def prime(): 
+    n=int(input("enter number:"))  #for getting input from user
+    flag=0
+    for i in range(2,n):  #range to divide the number
+        if n%i==0:       
+            flag=1
+            break
+    if n==1:
+        print("not prime or composite")
+    elif flag==0:
+        print("prime")
+    else:
+        print("composite")
+prime()
